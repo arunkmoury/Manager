58faf65732030ff8d2c7d30c7274777dd357c1cd
Arun Kumar <arunkmoury@gmail.com>
2018-03-12 16:43:25 +0530

    User can delete employee

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate , employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { employeeUpdate , employeeSave, employeeDelete } from '../actions';
+import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
 
+    state = { showModal: false}
     componentWillMount(){
         // Taking all the attributes employee and stuffing them prefilled value of elements
         _.each(this.props.employee, (value, prop) => {
@@ -14,10 +16,25 @@ class EmployeeEdit extends Component {
         });
     }
 
+    onTextPress(){
+        const { phone, shift } = this.props;
+
+        Communications.text(phone, `Your upcomming shift is on ${shift}`);
+    }
+
     onButtonPress(){
         const { name, phone, shift } = this.props;
 
         this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
+    }
+
+    onAccept(){
+        const { uid } = this.props.employee;
+        this.props.employeeDelete({ uid });
+    }
+
+    onDecline() {
+        this.setState({ showModal: false })
     }
     render() {
         return (
@@ -28,6 +45,23 @@ class EmployeeEdit extends Component {
                         Save Changes
                     </Button>
                 </CardSection>
+                <CardSection>
+                    <Button onPress={this.onTextPress.bind(this)} >
+                        Text Schedule
+                    </Button>
+                </CardSection>
+
+                <CardSection>
+                    <Button onPress={() => this.setState({ showModal: !this.state.showModal})}> Fire Employee</Button>
+                </CardSection>
+
+                <Confirm
+                    visible={this.state.showModal}
+                    onAccept={this.onAccept.bind(this)}
+                    onDecline={this.onDecline.bind(this)}
+                >
+                    Are you sure you want to delete this
+                </Confirm>
             </Card>
         );
     }
@@ -39,4 +73,4 @@ const mapStateToProp = (state) => {
     return { name, phone, shift };
 }
 
-export default connect(mapStateToProp, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProp, { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit);
